27a86e4bb66bc6415a9f019a77a7e7e10df64601
unknown <abhishekk.meena.min16@gmail.com>
2025-08-31 17:59:47 +0530

    changed main file

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 from scraper import scrape_tweets
-from parser import load_raw_data, parse_tweets, save_to_parquet
-from analyzer import generate_signals
+
 from visualizer import plot_graph
 
 def main():
@@ -8,6 +7,7 @@ def main():
     print("tweets have been scraped and saved in tweets.parquet file")
     
     plot_graph()
+    print("graph is plotted")
     
     
     
